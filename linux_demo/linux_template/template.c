e7e5eef24846ca65f6fcd27bfb415a6082e743f5
tony.chen <tony.chen@mstarsemi.com>
2017-12-24 20:17:18 +0800

    Add linux  C File demo template [red flag] = y/n

---
+++ linux_demo/linux_template/template.c
@@ -0,0 +1,29 @@
+/*******************************************************************************
+  * Copyright(C),2017,Mstar semi.co.,Ltd
+  * @file Namev : 
+  * @author  tony.chen@mstarsemi.com
+  * @version V1.0.0
+  * @date     
+  * @brief
+  * @History
+*******************************************************************************/
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// MI must define before mi_common.h 
+#define  MI_ENABLE_DBG  true
+#define  DBG_LEVEL  MI_DBG_ALL
+
+#include "mi_common.h"
+
+int main(int argc,char * argv[])
+{
+    MI_PRINT("This is my first demo! \n");
+    return 0;
+}
