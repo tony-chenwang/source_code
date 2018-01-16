41590a976cd1a0915c9eff61e3f8e293b9b1c293
tony.chen <tony.chen@mstarsemi.com>
2018-01-16 20:14:21 +0800

    move headfile to root dir [red flag] = y/n

--- linux_demo/linux_template/template.c
+++ linux_demo/linux_template/template.c
@@ -31,15 +31,20 @@ int main(int argc,char * argv[])
     MI_U32 ValueC = 300;
 	MI_U32 ValueMAX = 0;
 	MI_U32 ValueMIN = 0;
-	
+   
+     while(1)
+     {
+        system("clear");	
 	MI_U32 ALIGNVALUE = 13+16;
 	MI_PRINT("The ALIGN value  is %d \n",ALIGN_DOWNTO_16(ALIGNVALUE));
 	MI_PRINT("The ALIGN value  is %d \n",MEM_ALIGN(ALIGNVALUE,16));
 
 	GETMAX(ValueMAX,ValueA,ValueB,ValueC)
-    MI_PRINT("The MAX value of A&B&C is %d \n",ValueMAX);
+        MI_PRINT("The MAX value of A&B&C is %d \n",ValueMAX);
 
 	GETMIN(ValueMIN,ValueA,ValueB,ValueC)
 	MI_PRINT("The MIN value of A&B&C is %d \n",ValueMIN);
-    return 0;
+        sleep(2);    
+    } 
+     return 0;
 }
