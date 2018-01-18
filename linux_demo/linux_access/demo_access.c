7b2a661048ef4f03ed852e94aec6eb6c3030e185
tony.chen <tony.chen@mstarsemi.com>
2018-01-18 21:10:18 +0800

    add linux access demo [red flag] = y/n

---
+++ linux_demo/linux_access/demo_access.c
@@ -0,0 +1,94 @@
+/*******************************************************************************
+  * Copyright(C),2018,Mstar semi.co.,Ltd
+  * @file Namev : 
+  * @author  tony.chen@mstarsemi.com
+  * @version V1.0.0
+  * @date   2018-01-18  
+  * @brief  test access function
+  * @History
+
+     #include<unistd.h>  
+	 int access(const char* pathname, int mode)
+
+     mode:
+        F_OK 值为0，判断文件是否存在
+		R_OK 值为4，判断对文件是否有读权限
+		W_OK 值为2，判断对文件是否有写权限
+		X_OK 值为1，判断对文件是否有读写权限
+
+        对于 R_OK W_OK X_OK 三种情况 可以进行或运算 用来实现对 读写权限的获取
+
+    如何 要求满足返回 0 
+
+
+  
+*******************************************************************************/
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// MI must define before mi_common.h 
+#define  MI_ENABLE_DBG  true
+#define  DBG_LEVEL  MI_DBG_ALL
+
+#include "mi_common.h"
+#include "mi_internal.h"
+
+void help_access(char *progname)
+{
+  fprintf(stderr, "-----------------------------------------------------------------------\n");
+  fprintf(stderr, "Usage: %s file path | demo test the file R & W & X & EXSIT \n" \
+                  " \n", progname);
+  exit(1);
+}
+
+
+int main(int argc,char * argv[])
+{
+    MI_PRINT(" This is my access demo! \n");
+	
+    MI_U8 * pGetFilename = NULL;
+
+
+    if(argc < 2)
+	{
+		help_access(argv[0]);
+	}
+
+    pGetFilename = strdup((MI_U8 *)argv[1]);
+    MI_PRINT("Now the file path is %s \n",pGetFilename);
+
+    MI_PRINT("euid = %d,egid = %d,uid = %d, gid = %d\n",geteuid(), getegid(), getuid(), getgid());
+	if( access((const char *)pGetFilename,R_OK) == 0 )
+	{
+		MI_PRINT("%s is Read OK \n",pGetFilename);
+    }
+
+ 	if( access((const char *)pGetFilename,W_OK) == 0 )
+	{
+		MI_PRINT("%s is write OK \n",pGetFilename);
+	}
+
+	 if( access((const char *)pGetFilename,X_OK) == 0 )
+	 {
+		 MI_PRINT("%s is excute OK \n",pGetFilename);
+	 }
+
+	 if( access((const char *)pGetFilename,F_OK) == 0 )
+	 {
+		 MI_PRINT("%s is exsit OK \n",pGetFilename);
+	 }
+	 else
+	 {
+		perror("The file donot exsit \n");
+	 }
+
+	 
+	 pause();
+     return 0;
+}
