f28891105bc48d1a55c67ef35fbc64ab0e31fd2c
tony.chen <tony.chen@mstarsemi.com>
2018-02-03 13:53:42 +0800

    modify linux file fd name [red flag] = y/n

--- linux_demo/linux_filefd/demo_fd.cpp
+++ linux_demo/linux_filefd/demo_fd.cpp
@@ -2,9 +2,18 @@
   * Copyright(C),2017,Mstar semi.co.,Ltd
   * @file Name
   * @author  tony.chen@mstarsemi.com
+  
   * @version V1.0.0
+  
   * @date  2017/12/15
-  * @brief
+  
+  * @brief  verify one process can open filefd up to 1024
+            & 0 1 2 is used for STDIN_FILENO  STDOUT_FILENO  STDERR_FILENO
+
+
+                if we close file descriptror 1 the new creat fd must be 1 
+            so we can use this method to redirect log
+            
   * @History
 *******************************************************************************/
 // we usually use this head file 
@@ -17,22 +26,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define  sz_filename "testfile.txt"
+#define  demo_filename "testfile.txt"
+
+
+#define  demo_EXLOG    1
 
 int main(int argc,char * argv[])
 {
     long int fd = -1;
     int i = 0;
- 
+
+#if (demo_EXLOG == 0)
+
 	while (1) 
 	{
 	    if ( i == 0)
-	    {
-            fd = open(sz_filename,O_RDONLY | O_CREAT,0x700);
+	    {   // note that we use 
+            fd = open(demo_filename,O_RDONLY | O_CREAT,0x700);
 	    }
 	    else
 	    {
-            fd = open(sz_filename,O_RDONLY,0x700); 
+            fd = open(demo_filename,O_RDONLY,0x700); 
 	    }
 	    
 	    if (0 > fd ) goto Fail;
@@ -41,7 +55,19 @@ int main(int argc,char * argv[])
 	    usleep(50000);
         i++;
 	}
+	
+#elif (demo_EXLOG == 1)
+
+     int demologfd = -1;
+
+     close(STDOUT_FILENO);
+     demologfd = open("demo_log.log", O_CREAT | O_RDWR, 0664);
+     printf("The demolog file fd is %d \n",demologfd);
+	 
+#endif
+
     printf("Cannot run into there\n");
+
     return 1;
 
 Fail:
