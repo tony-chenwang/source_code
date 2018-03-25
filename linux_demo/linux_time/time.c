88c05dafb29fdda5b01e501c79ff56a3cb81e214
tony.chen <tony.chen@mstarsemi.com>
2018-03-25 15:14:49 +0800

    1.Add asctime & clock_gettime func

--- linux_demo/linux_time/time.c
+++ linux_demo/linux_time/time.c
@@ -6,6 +6,16 @@
   * @date 2017-12-18
   * @brief
   * @History
+		获取时间:
+		time  返回值为 到1970 年之间的秒数
+     
+		time(&timep); 传入为 time_t 结构
+		locatime(&timep) 返回为 struct tm 结构
+		asctime() 将 struct tm 结构 转换为字符串
+		ctime    直接将 time_t 结构转换为字符串
+
+        gettimeofday   struct  timeval  struct  timezone 结构
+
 *******************************************************************************/
 #include <sys/types.h>
 #include <unistd.h>
@@ -58,9 +68,17 @@ int main(int argc,char * argv[])
         tm_p = localtime(&timep);
         printf("Year: %d Month:%d,Day:%d \n", (1900+tm_p->tm_year),(1+tm_p->tm_mon), tm_p->tm_mday);   
         printf("%s %d:%d:%d \n", wday[tm_p->tm_wday],tm_p->tm_hour, tm_p->tm_min, tm_p->tm_sec);
+ 
+        // 使用这个函数可以将 struct tm *tm_p  转换为 字符串的格式
+        printf("curent time string %s \n",asctime(tm_p));
 
+        // local time 的 逆运算，将时间格式转换为 秒为单位
+        printf("curent time in seconds is %x \n",mktime(tm_p));
 
-
+        //直接将 time_t 结构转化为字符串
+        printf("curent time in string in ctime is %s \n",ctime(&timep));
+  
+        
 		Ret = gettimeofday(&tv,&tz);
 		if ( Ret < 0 ) goto FAIL;
 
