137fff930429094388da82a094c3863a3cea6705
tony.chen <m18516396750163.com>
2019-01-13 16:07:36 +0800

    modify linux pptr demo for style & comment

--- linux_demo/linux_pptr/demo_pptr.c
+++ linux_demo/linux_pptr/demo_pptr.c
@@ -5,8 +5,8 @@
   * @version V1.0.0
   * @date     2018-01-24
   * @brief
-		1. 实参和形参之间是单向的值传递，实参不能通过函数改变其本身的值
-		2. 如果要改变 指针的值  可以通过 函数返回值 和 指针的指针来实现
+		1. real para only pass to seeming para ne way
+		2. change pointer value using pptr or function return value
 
   
   * @History
@@ -27,8 +27,6 @@
 #include "mi_common.h"
 #include "mi_internal.h"
 
-
-
 /****************************************************
   * @functionName demo_getmemory
   * @brief  this function says we cannot change real para 
@@ -37,27 +35,25 @@
 ****************************************************/
 void demo_getmemory( int *p, MI_U16 length )
 {
-	p = (int *)malloc(sizeof(int)*length);
+    p = (int *)malloc(sizeof(int)*length);
     ASSERT(p);
 }
 
-
-
 int * demo_getmemoryptr(MI_U16 length)
 {
-	int *p = (int *)malloc(sizeof(int)*length);
+    int *p = (int *)malloc(sizeof(int)*length);
     ASSERT(p);
 
-	return p;
+    return p;
 }
 
 
 int * demo_getmemorypptr(int **p,MI_U16 length)
 {
-	*p = (int *)malloc(sizeof(int)*length);
+    *p = (int *)malloc(sizeof(int)*length);
     ASSERT(p);
 
-	return *p;
+    return *p;
 }
 
 
@@ -71,18 +67,18 @@ int main(int argc,char * argv[])
     MI_PRINT("The address of pointer is %p \n",pointer);
 
 
-	pointer = NULL;
+    pointer = NULL;
     pointer = demo_getmemoryptr(100);
     MI_PRINT("The address of pointer is %p \n",pointer);
     memset(pointer,0xAA,100);
 
 
 
-	pointer = NULL;
-	int * eRetpointer = NULL;
-    eRetpointer = demo_getmemorypptr(&pointer,100);
-	MI_PRINT("The address of pointer is %p \n",pointer);
-	MI_PRINT("The address of eRetpointer is %p \n",eRetpointer);
+   pointer = NULL;
+   int * eRetpointer = NULL;
+   eRetpointer = demo_getmemorypptr(&pointer,100);
+   MI_PRINT("The address of pointer is %p \n",pointer);
+   MI_PRINT("The address of eRetpointer is %p \n",eRetpointer);
 
      return 0;
 }
