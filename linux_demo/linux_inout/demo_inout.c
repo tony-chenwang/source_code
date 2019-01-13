62e297f2143d1504ed5e5ac204fa1416f3038238
tony.chen <m18516396750163.com>
2019-01-13 15:35:19 +0800

    modify inout demo for style & coomment

--- linux_demo/linux_inout/demo_inout.c
+++ linux_demo/linux_inout/demo_inout.c
@@ -5,9 +5,9 @@
   * @version V1.0.0
   * @date     
   * @brief
-  			getchar
-  			putchar
-  			puts
+  		getchar
+  		putchar
+  		puts
   			
   * @History
 *******************************************************************************/
@@ -27,47 +27,44 @@
 #include "mi_common.h"
 #include "mi_internal.h"
 
-
 #define demo_module 2
 
 int main(int argc,char * argv[])
 {
     MI_PRINT("This is my basic in out demo! %s builddate:%s %s\n",__FUNCTION__,__DATE__,__TIME__);
     char ch = 0;
-	char ch_t = 1;
+    char ch_t = 1;
 
 #if (demo_module == 1)
     do
-	{
-		ch = getchar();  // only get \n the function will return;
-		//ch = getch();
-		//putchar('A');  // 验证 getchar 只有 遇到 \n 才会返回
-	    putchar(ch);
-	}while( ch != '\n');
+    {
+	ch = getchar();  // only get \n the function will return;
+	//ch = getch();
+	//putchar('A'); 
+	putchar(ch);
+    }while( ch != '\n');
 
     putchar('A');
-	putchar('\n');  // putchar 每次只能输出一个字符
-	puts("this is my demo"); // 注意 put 函数会自动换行 如果加上 \n 
+    putchar('\n');  // putchar print a char one time
+    puts("this is my demo"); // puuts will add \n automatically
 	
 #elif (demo_module == 2)
 
-	MI_PRINT("char demo  %c %c \n",'A',48);
+	MI_PRINT("char demo  %c %c \n",'A',48); // both char & int is compatible
 	MI_PRINT("int demo %d,%ld \n",2018,20182018ul);
 	
-	// TODO:  only print previous number
+	// TODO:  only print previous numb	
 	MI_PRINT("int demo %-3d,%3ld \n",2018,20182018ul);
 
 	MI_PRINT("\t speical char demo \n");
-	MI_PRINT("float demo %f  %3.3f  %3.9f\n",1/3.0,1/3.0,1/3.0);
+	MI_PRINT("float demo %f  %3.3f  %3.9f\n",1/3.0,1/3.0,1/3.0); // .3 means only save 3 ponit number
 
-    MI_PRINT("Hex : %x %#x,DEC: %d,eight : %o %#o\n",100,100,100,100,100);
+        MI_PRINT("Hex : %x %#x,DEC: %d,eight : %o %#o\n",100,100,100,100,100); 
 
 	MI_PRINT("pointer demo %p %p\n",&ch,&ch_t);
 
-	MI_PRINT("float demo demo %e %E \n",1/3.0,1/3.0);
+	MI_PRINT("float demo demo %e %E \n",1/3.0,1/3.0); // git config --global core.whitespace cr-at-eol
 #endif
 
-
-	
      return 0;
 }
